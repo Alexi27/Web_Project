854fd425bc9158a1540ec20ffa85bc1cc4b823f3
Alexi27 <alexi27cuellar@hotmail.fr>
2017-12-20 21:48:33 +0100

    ajout mode mini-chat avec la table sql phase test

---
+++ minichat_post.php
@@ -0,0 +1,18 @@
+<?php
+// Connexion à la base de données
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=projetweb;charset=utf8', 'Alexi27', 'Alexi2427');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+
+// Insertion du message à l'aide d'une requête préparée
+$req = $bdd->prepare('INSERT INTO minichat (dateetheure, pseudo, message) VALUES (CURRENT_TIMESTAMP(), ?, ?)');
+$req->execute(array($_POST['pseudo'], $_POST['message']));
+
+// Redirection du visiteur vers la page du minichat
+header('Location: minichat.php');
+?>
--- page2.php
+++ page2.php
@@ -1,7 +1,39 @@
 <html>
 <body>
+    
+<?php
+$mail = $_POST['mail'];
+$password = $_POST['password'];
+$nom = $_POST['nom'];
+$prenom = $_POST['prenom'];
+$genre = $_POST['genre'];
+$tel = $_POST['tel'];
+$birthday = $_POST['birthday'];
+echo $_POST['mail'];
+echo $_POST['password'];
+echo $_POST['nom'];
+echo $_POST['prenom'];
+echo $_POST['genre'];
+echo $_POST['tel'];
+echo $_POST['birthday'];?>
+
 <?php $bdd = new PDO('mysql:host=localhost;dbname=projetweb;charset=utf8', 'Alexi27', 'Alexi2427');?>
-    <table>
+<! On ajoute une entrée dans la table >
+<?php
+$req = $bdd->prepare('INSERT INTO projetweb(mail, password, nom, prenom, genre, tel, birthday) VALUES(:mail, :password, :nom, :prenom, :genre, :tel, :birthday)');
+$req->execute(array(
+	'mail' => $mail,
+	'password' => $password,
+	'nom' => $nom,
+	'prenom' => $prenom,
+	'genre' => $genre,
+    'tel' => $tel,
+    'birthday' => $birthday
+	));
+
+echo 'Information bien enregistré!';
+
+?><table>
     <tr>
         <td>
             email:
