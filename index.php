2d23614febf66e6b1703d67edd709748d1d1cbbc
Alexi27 <alexi27cuellar@hotmail.fr>
2018-01-19 17:43:50 +0100

    ajout bouton top et modif css

--- index.php
+++ index.php
@@ -10,118 +10,6 @@ session_start(); // On démarre la session
         <link rel="stylesheet" href="index.css">
         <title>Premiers test</title>
 
-        <style>
-            body,
-            html {
-                height: 100%;
-            }
-
-            .parallax {
-                background-image: url('https://wallpaperscraft.com/image/inkwell_pen_ink_77039_2048x1152.jpg');
-                height: 100%;
-                background-attachment: fixed;
-                background-position: center;
-                background-repeat: no-repeat;
-                background-size: cover;
-                background-color:
-            }
-
-            .bandetete {
-                float: center;
-                width: 100%;
-                height: 5%;
-                text-align: center;
-                background-color: grey;
-                font-size: 16px;
-            }
-
-            .dropbtn {
-
-                color: white;
-                font-size: 16px;
-                border: none;
-                cursor: pointer;
-            }
-
-            .dropdown {
-                position: relative;
-                display: inline-block;
-            }
-
-            .dropdown-content {
-                display: none;
-                position: absolute;
-                right: 0;
-                background-color: #f9f9f9;
-                min-width: 160px;
-                box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-                z-index: 1;
-            }
-
-            .dropdown-content a {
-                color: black;
-                padding: 12px 16px;
-                text-decoration: none;
-                display: block;
-            }
-
-            .dropdown-content a:hover {
-                background-color: #f1f1f1
-            }
-
-            .dropdown:hover .dropdown-content {
-                display: block;
-            }
-
-            .dropdown:hover .dropbtn {
-                background-color: #3e8e41;
-            }
-
-            .structure1 {
-                margin: auto;
-                float: center;
-                height: 600px;
-                width: 100%;
-                background-color: black;/* rgba(0, 0, 0, 0);*/
-            }
-
-            #nomtitre {
-                margin: auto;
-                text-align: center;
-                vertical-align: middle;
-            }
-
-            .image2 {
-                -webkit-transform: scale(1);
-                transform: scale(1);
-                -webkit-transition: .3s ease-in-out;
-                transition: .3s ease-in-out;
-            }
-
-            .image2:hover {
-                -webkit-transform: scale(1.3);
-                transform: scale(1.3);
-            }
-            /* Opacité */
-
-            .image1text h4 {
-                padding-top: 10px;
-            }
-            
-            .image1text {
-                position: relative;
-                bottom: 100px;
-                background-color: rgba(0,0,0,0.5);
-                color: white;
-                padding-left: 20px;
-                padding-right: 20px;
-                display: block;
-                text-align: center;
-
-            .image1:hover {
-                opacity: .4;
-            }
-        </style>
     </head>
 
     <body>
@@ -132,7 +20,7 @@ session_start(); // On démarre la session
             <div class="dropdown-content">
 
                 <div>
-                    <?php 
+                    <?php  
                         if(isset($_SESSION['pseudo']))// on verrifie qu'il y a quelque chose dans le pseudo du tableau session  
                         { //<img src='C:\wamp64\www\Projet_Web\user.png'>
                             if(strlen($_SESSION['pseudo'] == 0))// si oui on check alors qu'il est pas egal a 0
@@ -153,77 +41,105 @@ session_start(); // On démarre la session
             </div>
         </div>
 
-        <h1 id="nomtitre">Navari Robin</h1>
+        <h1 id="nomtitre">BOOOOOOM</h1>
 
         <div class "leshastags">
             <strong>#Productible.rn #Design #eko</strong>
-            <a href="http://www.unitag.io/qreator/generate?setting=%7B%22EYES%22%3A%7B%22EYE_TYPE%22%3A%22Simple%22%7D%2C%22E%22%3A%22M%22%2C%22BODY_TYPE%22%3A0%2C%22LAYOUT%22%3A%7B%22COLORBG%22%3A%22ffffff%22%2C%22COLOR1%22%3A%22000000%22%7D%7D&data=%7B%22TYPE%22%3A%22vcard%22%2C%22DATA%22%3A%7B%22FIRSTNAME%22%3A%22Robin%22%2C%22NAME%22%3A%22Navari%22%2C%22EMAIL%3BINTERNET%22%3A%22navari9203%40outlook.com%22%2C%22EMAIL%3BHOME%3BINTERNET%22%3A%22%22%2C%22TEL%3BCELL%22%3A%220681746320%22%2C%22TEL%3BWORK%22%3A%22%22%2C%22TEL%3BHOME%22%3A%22%22%2C%22TEL%3BHOME%3BFAX%22%3A%22%22%2C%22URL%22%3A%22%22%2C%22TITLE%22%3A%22%22%2C%22ORG%22%3A%22%22%2C%22ADR%22%3A%22%22%2C%22PC%22%3A%22%22%2C%22CITY%22%3A%22%22%2C%22COUNTRY%22%3A%22%22%2C%22NOTE%22%3A%22%22%7D%7D ">
-                <img src="http://www.unitag.io/qreator/generate?setting=%7B%22EYES%22%3A%7B%22EYE_TYPE%22%3A%22Simple%22%7D%2C%22E%22%3A%22M%22%2C%22BODY_TYPE%22%3A0%2C%22LAYOUT%22%3A%7B%22COLORBG%22%3A%22ffffff%22%2C%22COLOR1%22%3A%22000000%22%7D%7D&data=%7B%22TYPE%22%3A%22vcard%22%2C%22DATA%22%3A%7B%22FIRSTNAME%22%3A%22Robin%22%2C%22NAME%22%3A%22Navari%22%2C%22EMAIL%3BINTERNET%22%3A%22navari9203%40outlook.com%22%2C%22EMAIL%3BHOME%3BINTERNET%22%3A%22%22%2C%22TEL%3BCELL%22%3A%220681746320%22%2C%22TEL%3BWORK%22%3A%22%22%2C%22TEL%3BHOME%22%3A%22%22%2C%22TEL%3BHOME%3BFAX%22%3A%22%22%2C%22URL%22%3A%22%22%2C%22TITLE%22%3A%22%22%2C%22ORG%22%3A%22%22%2C%22ADR%22%3A%22%22%2C%22PC%22%3A%22%22%2C%22CITY%22%3A%22%22%2C%22COUNTRY%22%3A%22%22%2C%22NOTE%22%3A%22%22%7D%7D"
-                    alt="QR Code - Robin NAVARI carte de visite"> </a>
-        </div>
-        <div class="parallax"></div>
-        <div class="bandetete">
-            <strong>Work</strong>
-        </div>
-        <div class="structure1">
-            <table>
-                <tr>
-                    <td>
-                        <div class="image1">
-                            <img src="./chaise/DSC1357.png" width="450" height="400" >
-                            <div class="image1text">
-                                <h4>Chaise</h4>
-                                <p>chaise design salon</p>
+
+
+            <img id="qrcode" src="http://www.unitag.io/qreator/generate?setting=%7B%22EYES%22%3A%7B%22EYE_TYPE%22%3A%22Simple%22%7D%2C%22E%22%3A%22M%22%2C%22BODY_TYPE%22%3A0%2C%22LAYOUT%22%3A%7B%22COLORBG%22%3A%22ffffff%22%2C%22COLOR1%22%3A%22000000%22%7D%7D&data=%7B%22TYPE%22%3A%22vcard%22%2C%22DATA%22%3A%7B%22FIRSTNAME%22%3A%22Robin%22%2C%22NAME%22%3A%22Navari%22%2C%22EMAIL%3BINTERNET%22%3A%22navari9203%40outlook.com%22%2C%22EMAIL%3BHOME%3BINTERNET%22%3A%22%22%2C%22TEL%3BCELL%22%3A%220681746320%22%2C%22TEL%3BWORK%22%3A%22%22%2C%22TEL%3BHOME%22%3A%22%22%2C%22TEL%3BHOME%3BFAX%22%3A%22%22%2C%22URL%22%3A%22%22%2C%22TITLE%22%3A%22%22%2C%22ORG%22%3A%22%22%2C%22ADR%22%3A%22%22%2C%22PC%22%3A%22%22%2C%22CITY%22%3A%22%22%2C%22COUNTRY%22%3A%22%22%2C%22NOTE%22%3A%22%22%7D%7D"
+                </a>
+
+
+            <div class="parallax"></div>
+            <div class="bandetete">
+                <strong>Work</strong>
+            </div>
+            <div class="triangle-down"></div>
+            <div class="structure1">
+                <table>
+                    <tr>
+                        <td>
+                            <div class="image1">
+                                <img src="./chaise/DSC1357.png" width="450" height="400 ">
+                                <div class="image1text">
+                                    <h4>Chaise</h4>
+                                    <p>chaise design salon</p>
+                                </div>
                             </div>
-                        </div>
-                    </td>
-                    <td>
-                        <img class="image2" src="./photocouteaux/DSC_1344.jpg" width="300" height="300" alt="Texte " title="Texte ">
-                    </td>
-                    <td>
-                        <img src="">
-                    </td>
-                    <td>
-                        <img src="">
-                    </td>
-                    <td>
-                        <img src="">
-                    </td>
-                </tr>
-            </table>
-        </div>
+                        </td>
+                        <td> 
+                            <img class="image2" src="./photocouteaux/DSC_1344.jpg" width="450" height="400" alt="Texte " title="Texte ">
+                            <div class="image2text">
+                                <h4>couteau</h4>
+                                <p>couteau design cuisine</p>
+                            </div>
+                        </td>
+                        <td>
+                            <img src="./photomaquettechocolat/DSC_1351.JPG" width="450" height="400" alt="Texte " title="Texte ">
+                            <div class="image2text">
+                                <h4>Maquette Chocolat</h4>
+                                <p>maquette design Chocolat</p>
+                            </div>
+                        </td>
+                        <td>
+                            <img src="">
+                        </td>
+                        <td>
+                            <img src="">
+                        </td>
+                    </tr>
+                </table>
+            </div>
 
-        <div class="parallax"></div>
-        <div class="bandetete">
-            <strong>About</strong>
-        </div>
-        <div class="structure1">
-            <table>
-                <tr>
-                    <td>
-                        <img class="image1" src="C:\wamp64\www\Projet_Web\chaise\DSC1357.png" width="400" height="300" alt="Texte " title="Texte "
-                        />
+            <div class="parallax"></div>
+            <div class="bandetete">
+                <strong>About</strong>
+            </div>
+            <div class="triangle-down"></div>
+            <div class="structure1">
+                <table>
+                    <tr>
+                        <td>
+                            <img class="image1" src="C:\wamp64\www\Projet_Web\chaise\DSC1357.png" width="400" height="300" alt="Texte " title="Texte "
+                            />
 
-                    </td>
-                    <td>
-                        <img class="image2" src="C:\wamp64\www\Projet_Web\photocouteaux\DSC_1344.jpg" width="300" height="300" alt="Texte " title="Texte ">
-                    </td>
-                    <td>
-                        <img src="">
-                    </td>
-                    <td>
-                        <img src="">
-                    </td>
-                    <td>
-                        <img src="">
-                    </td>
-                </tr>
-            </table>
+                        </td>
+                        <td>
+                            <img class="image2" src="C:\wamp64\www\Projet_Web\photocouteaux\DSC_1344.jpg" width="300" height="300" alt="Texte " title="Texte ">
+                        </td>
+                        <td>
+                            <img src="">
+                        </td>
+                        <td>
+                            <img src="">
+                        </td>
+                        <td>
+                            <img src="">
+                        </td>
+                    </tr>
+                </table>
+            </div>
         </div>
+        <button onclick="topFunction()" id="boutontop">Top</button>
+        <script>
+//Quand on descend de 20px on affiche le bouton 
+window.onscroll = function() {scrollFunction()};
 
-        <a href="./formulaire.html">inscription</a>
-        <a href="./minichat.php">chat</a>
-        </div>
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("boutontop").style.display = "block";
+    } else {
+        document.getElementById("boutontop").style.display = "none";
+    }
+}
+
+// Quand on clique su le bouton on remonte en haut de la page
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+</script>
     </body>
 
     </html>
